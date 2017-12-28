7be6e1d1bb2fd4d2774fbc26578e16078ba45c7a
Oski <31088167+ShockDoctor@users.noreply.github.com>
2017-12-28 03:13:56 -0500

    Add files via upload
    This script can be run in the QGIS console or through an IDE to obtain the primary and market areas for a specific attribute (i.e mall).

---
+++ Selecting_Each_Mall.py
@@ -0,0 +1,175 @@
+from qgis.core import *
+from qgis.gui import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+def run_script(iface):
+
+    # Make a copy of the huff model layer to work with so that all changes are made to copy only keeping original data
+    # intact
+    ori_huff_model = QgsVectorLayer("C:/Users/Oski/Py_internship/Huff_Model_Oski/Data/HuffEDExp1TorCMAxSC13FlSp.shp", "polygon", "ogr")
+
+    huff_model = QgsVectorLayer("Polygon?crs=epsg:4326", "Huff_Model", "memory")
+    huff_model_data = huff_model.dataProvider()
+
+    huff_model.startEditing()
+
+    attr = ori_huff_model.dataProvider().fields().toList()
+    huff_model_data.addAttributes(attr)
+    huff_model.updateFields()
+
+    feat = QgsFeature()
+    for elem in ori_huff_model.getFeatures():
+        feat.setGeometry(elem.geometry())
+        feat.setAttributes(elem.attributes())
+        huff_model.addFeatures([feat])
+        huff_model.updateExtents()
+
+    # Checks that the current layer is valid
+    if huff_model.isValid():
+        print "Model is valid."
+    else:
+        print "Invalid Model."
+
+    for field in huff_model.fields():
+        field_id = huff_model.fieldNameIndex(field.name())
+        print ">>> Field name: {} Field ID: {}".format(field.name(), field_id)
+        if "HiSC1009" == field.name():
+            mall = huff_model.fieldNameIndex(field.name())
+            print "<<< mall ID: {}".format(mall)
+            break
+
+    print "XXXX: {}".format(mall)
+
+    # This is where you choose the mall that you want the market areas for. Currently accepts an index number,
+    # which is column number in the attribute table. I.e 1 for the first column aka first mall, 2 for the second column
+    # aka second mall and so forth
+    qid = QInputDialog()
+
+    title = "Which mall would you like to find the market area for?"
+    label = "Name: "
+    mode = QLineEdit.Normal
+    default = "<mall name here>"
+
+    text, ok = QInputDialog.getText(qid, title, label, mode, default)
+    col_num = int(text)
+    print col_num
+
+    # Create two new fields that will only show the primary and secondary market areas based on selected probabilities
+    primary = QgsField('Primary', QVariant.Double)
+    huff_model.addAttribute(primary)
+    index_pri = huff_model.fieldNameIndex('Primary')
+
+    secondary = QgsField('Secondary', QVariant.Double)
+    huff_model.addAttribute(secondary)
+    index_sec = huff_model.fieldNameIndex('Secondary')
+
+    for feature in huff_model.getFeatures():
+        probability = feature.attributes()
+        ctuid = feature["CTUID"]
+        if not probability[col_num]:
+            print ("This mall does not exist")
+        else:
+            # Primary market values will go into this column
+            if probability[col_num] >= .6:
+                print("Primary market area: {} ({})".format(ctuid, probability[col_num]))
+                huff_model.changeAttributeValue(feature.id(), index_pri, probability[col_num])
+            # Secondary market values will go into this column
+            if .4 <= probability[col_num] < .6:
+                print("Secondary market area: {} ({})".format(ctuid, probability[col_num]))
+                huff_model.changeAttributeValue(feature.id(), index_sec, probability[col_num])
+
+    # Delete all other columns except for the CTUID, Primary, Secondary columns
+    fields = []
+
+    fieldnames = {'CTUID', 'Primary', 'Secondary'}
+    for field in huff_model.fields():
+        if field.name() not in fieldnames:
+            fields.append(huff_model.fieldNameIndex(field.name()))
+            print(fields.append(huff_model.fieldNameIndex(field.name())))
+
+    huff_model.deleteAttributes(fields)
+
+    # Delete all other features (rows) that do not have a probability in the primary and the secondary column
+    expr = QgsExpression("\"Primary\" is NULL and \"Secondary\" is NULL")
+    for f in huff_model.getFeatures(QgsFeatureRequest(expr)):
+        huff_model.deleteFeature(f.id())
+
+    huff_model.commitChanges()
+
+    # Making sure that only one copy layer exists at a time, if running the script multiple times with different malls
+    layers = QgsMapLayerRegistry.instance().mapLayers()
+    for name, layer in layers.iteritems():
+        if layer.name() == "Huff_Model":
+            print "It exists"
+            if "Huff_Model" in name:
+                QgsMapLayerRegistry.instance().removeMapLayer(layer)
+        else:
+            print "Does not exist"
+
+    # Add newly formatted layer to map
+    reg = QgsMapLayerRegistry.instance()
+    reg.addMapLayer(huff_model)
+
+    # Join the census layer to the newly created layer to show specific demographic data. Currently the full table is
+    # joined but specific columns can be chose if an entire table join is unnecessary.
+    targetLyr = QgsMapLayerRegistry.instance().mapLayersByName("Huff_Model")[0]
+    censusLyr = QgsMapLayerRegistry.instance().mapLayersByName("TorontoCMA_2006census_region")[0]
+
+    root = QgsProject.instance().layerTreeRoot()
+
+    mytargetLyr = root.findLayer(targetLyr.id())
+    targetClone = mytargetLyr.clone()
+    parent = mytargetLyr.parent()
+    parent.insertChildNode(0, targetClone)
+    parent.removeChildNode(mytargetLyr)
+
+    mycensusLyr = root.findLayer(censusLyr.id())
+    censusClone = mycensusLyr.clone()
+    parent = mycensusLyr.parent()
+    parent.insertChildNode(1, censusClone)
+    parent.removeChildNode(mycensusLyr)
+
+    # Set properties for the join
+    targetField = 'CTUID'
+    inField = 'CTUID'
+    joinObject = QgsVectorJoinInfo()
+    joinObject.joinLayerId = censusLyr.id()
+    joinObject.joinFieldName = inField
+    joinObject.targetFieldName = targetField
+    print(targetLyr.addJoin(joinObject))  # You should get True as response.
+    targetLyr.addJoin(joinObject)
+
+    # Make sure that the newly created and joined layer is the active layer.
+    huff_model = iface.activeLayer()
+
+    # Create the attribute using the map composer in QGIS and show the output in PDF format.
+    mapRenderer = iface.mapCanvas().mapRenderer()
+    c = QgsComposition(mapRenderer)
+    c.setPlotStyle(QgsComposition.Print)
+    x, y = 0, 0
+    w, h = c.paperWidth(), c.paperHeight()
+    composerMap = QgsComposerMap(c, x, y, w, h)
+
+    table = QgsComposerAttributeTable(c)
+    table.setComposerMap(composerMap)
+    table.setVectorLayer(huff_model)
+    table.setMaximumNumberOfFeatures(huff_model.featureCount())
+    c.addItem(table)
+
+    printer = QPrinter()
+    printer.setOutputFormat(QPrinter.PdfFormat)
+    printer.setOutputFileName("C:/Users/Oski/Py_internship/Huff_Model_Oski/Outputs/out.pdf")
+    printer.setPaperSize(QSizeF(c.paperWidth(), c.paperHeight()), QPrinter.Millimeter)
+    printer.setFullPage(True)
+
+    pdfPainter = QPainter(printer)
+    paperRectMM = printer.pageRect(QPrinter.Millimeter)
+    paperRectPixel = printer.pageRect(QPrinter.DevicePixel)
+    c.render(pdfPainter, paperRectPixel, paperRectMM)
+    pdfPainter.end()
+
+
+
+
+
